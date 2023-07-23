6543f28a1b51421585f8bfb2b6d0fc7075f31c84
EC2 Default User <ec2-user@ip-10-0-15-139.ec2.internal>
2023-07-23 12:49:10 +0000

    spring boot with lambda and SAM working code

---
+++ src/main/java/my/sample/service/controller/PingController.java
@@ -0,0 +1,20 @@
+package my.sample.service.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@EnableWebMvc
+public class PingController {
+    @RequestMapping(path = "/ping", method = RequestMethod.GET)
+    public Map<String, String> ping() {
+        Map<String, String> pong = new HashMap<>();
+        pong.put("ping", "Hello, World!");
+        return pong;
+    }
+}
